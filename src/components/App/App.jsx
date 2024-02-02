bc1f3c5df2a3970c830bba351f3a0bb3db6790a6
Chapelk1 <klepach154789@gmail.com>
2024-02-02 22:46:28 +0200

    +

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,24 +1,23 @@
-
-import { Routes, Route } from "react-router-dom"
-import HomePage from "page/HomePage/HomePage";
+import { Routes, Route, useLocation } from 'react-router-dom';
+import HomePage from 'page/HomePage/HomePage';
 import CatalogPage from 'page/CatalogPage/CatalogPage';
-import FavoritesPage from "page/FavoritesPage/FavoritesPage";
-import {Layout} from 'components/SharedLayout/Layout'
+import FavoritesPage from 'page/FavoritesPage/FavoritesPage';
+import { Layout } from 'components/SharedLayout/Layout';
 
 export const App = () => {
-    
+  const location = useLocation();
+  console.log(location);
 
-
-    return (
-      <>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<HomePage />} />
-            <Route path="/catalog" element={<CatalogPage />} />
-            <Route path="/favorites" element={<FavoritesPage />} />
-            <Route path="*" element={<HomePage />} />
-          </Route>
-        </Routes>
-      </>
-    );
-}
+  return (
+    <>
+      <Routes location={location} key={location.pathname}>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route path="/catalog" element={<CatalogPage />} />
+          <Route path="/favorites" element={<FavoritesPage />} />
+          <Route path="*" element={<HomePage />} />
+        </Route>
+      </Routes>
+    </>
+  );
+};
